0c0ad233ba9cec88a5ec6a2903ae2ef81d7567a1
ElTracThor <90599499+ElTracThor@users.noreply.github.com>
2023-05-22 14:32:57 +0200

    ajout footer

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,7 +2,9 @@
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
   import NavBar from "./components/navbar.svelte"
+  import Footer from "./components/footer.svelte"
 </script>
 
 <NavBar />
 <Router {routes} />
+<Footer />
---
+++ frontend/src/components/footer.svelte
@@ -0,0 +1,6 @@
+<footer>
+    <div class="footer-content">
+      <p>&copy; <span id="currentYear"></span> Made by groupe 2</p>
+    </div>
+  </footer>
+  
