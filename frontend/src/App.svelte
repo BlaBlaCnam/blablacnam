bd402c631d460d2c1852bb608318429e1212e430
MarcFaussurier <faussurier.marc@icloud.com>
2023-05-22 14:13:49 +0200

    up

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
+  import NavBar from "./components/navbar.svelte"
 </script>
 
+<NavBar />
 <Router {routes} />
