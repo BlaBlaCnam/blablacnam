b62621c6661a0f751ceaae1f31bb10272cbd8ce5
thomas <thomas.vessiller.tv@gmail.com>
2023-05-23 16:10:18 +0200

    favicon update+logo update+header update

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -1,12 +1,6 @@
-<script>
-
-  var  year = new Date().getFullYear();
-
-</script>
-
 <footer>
   <div class="footer-content">
-    <p>&copy; {year} Made by groupe 2 | <a href="/footerInfo/CGU">CGU</a></p>
+    <p>&copy; {new Date().getFullYear()} Made by groupe 2 | <a href="/footerInfo/CGU">CGU</a></p>
   </div>
 </footer>
 
