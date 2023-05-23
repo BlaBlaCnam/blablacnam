5c092b398d03432848423edf642a94525d809643
oceane tardy <tardy.oceane42@gmail.com>
2023-05-23 16:10:28 +0200

    Ajout page modif info utilisateur

---
+++ frontend/src/pages/user/modifinfouser.svelte
@@ -0,0 +1,20 @@
+
+ <h1>Modification Informations utilisateurs</h1>
+
+<div class="info-container">
+    <h2 class="info-heading">Information utilisateurs</h2>
+    <form>
+      <input class="inputpage" type="text" placeholder="Nom" required>
+      <input class="inputpage" type="text" placeholder="Prénom" required>
+      <input class="inputpage" type="text" placeholder="Téléphone" required>
+      <input class="inputpage" type="date" placeholder="Date de naissance" required>
+      <input class="inputpage" type="text" placeholder="Site de formation" required>
+      <button class="btnpage" type="submit">Modifier</button>
+    </form>
+  </div>
+  
+
+
+
+
+
