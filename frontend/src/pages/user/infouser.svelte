d1551255f76f9b41c91e039fccf6792bce798b8b
oceane tardy <tardy.oceane42@gmail.com>
2023-05-23 16:11:38 +0200

    Ajout page info user

---
+++ frontend/src/pages/user/infouser.svelte
@@ -0,0 +1,24 @@
+
+
+<div class="info-container">
+    <h2 class="info-heading">Information utilisateurs</h2>
+      <p> Nom: </p>
+      <p> Prénom: </p>
+      <p> Téléphone: </p>
+      <p> Date de Naissance: </p>
+      <p> Site de formatiion : </p>
+    <h3>Adresse</h3>
+          <p> Numéro: </p>
+      <p> Rue: </p>
+      <p> Code postal: </p>
+      <p> Ville: </p>
+
+        <button class="btn btn-secondary" >Supprimer le compte</button>
+  <button class="btn btn-secondary"> Modifier mes données</button>
+
+  </div>
+
+
+
+
+
