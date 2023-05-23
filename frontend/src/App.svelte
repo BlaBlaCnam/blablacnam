5814048c33973120abf256a4ffe7bd8a12fe428d
MarcFaussurier <faussurier.marc@icloud.com>
2023-05-23 11:03:09 +0200

    no navbar and footer on login and register pages

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,33 @@
 <script>
-  import { Router } from "@roxi/routify";
+  import { Router, page } from "@roxi/routify";
   import { routes } from "../.routify/routes";
   import NavBar from "./components/navbar.svelte"
   import Footer from "./components/footer.svelte"
+  import { onMount } from 'svelte';
+
+  let currentPage;
+
+onMount(() => {
+  // Subscribe to changes in the $page store
+  const unsubscribe = page.subscribe(($page) => {
+    currentPage = $page.path;
+  });
+
+  // Clean up the subscription when the component is destroyed
+  return unsubscribe;
+});
+
 </script>
 
-<NavBar />
+
+
+{#if currentPage != "/Connexion/connexion" && currentPage != "/Connexion/inscription"}
+  <NavBar />
+{/if}
+
 <Router {routes} />
-<Footer />
+
+{#if currentPage != "/Connexion/connexion" && currentPage != "/Connexion/inscription"}
+  <Footer />
+{/if}
 
