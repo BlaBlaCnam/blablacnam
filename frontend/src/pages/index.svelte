dea86c89b1f59340ddcfb4d3467e734fe9f35176
oceane tardy <tardy.oceane42@gmail.com>
2023-05-24 14:44:40 +0200

    Modif page accueil récup

--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -6,38 +6,75 @@
 </script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <main class="container">
-    <div class="row ">
-        <div class="col-md-4">
 
-            <div class="search-container row justify-content-center d-flex flex-wrap">
 
-                <img src="/BLABLACNAM.png" alt="Logo" class="logo">
-                <h2 class="login-heading">Rechercher un trajet</h2>
-                <form>
-                    <input  type="text" placeholder="Ville de départ" required>
-                    <input  type="text" placeholder="Ville d'arrivée" required>
-                    <input  type="date" placeholder="Date" required>
-                    <input  type="number" placeholder="Nombre de place" required>
+    <div class="row form-search">
+        <h2 class="">Rechercher un trajet - Ville -> CNAM</h2>
 
+        <form>
+            <div class="row">
+                <div class="col">
+                    <input type="text" placeholder="Ville de départ" required>
+                </div>
+                <div class="col">
+                    <input type="text" placeholder="CNAM d'arrivé" required>
+                </div>
+                <div class="col">
+                    <input type="date" placeholder="Date" required>
+                </div>
+                <div class="col">
+                    <input type="number" placeholder="Nombre de place" required>
+                </div>
+                <div class="col">
                     <button type="submit">Rechercher</button>
-                </form>
+
+                </div>
             </div>
 
+
+        </form>
+    </div>
+
+    <div class="row form-search">
+        <h2 class="">Rechercher un trajet - CNAM -> Ville</h2>
+        <form>
+            <div class="row">
+                <div class="col">
+                    <input type="text" placeholder="Ville de départ" required>
+                </div>
+                <div class="col">
+                    <input type="text" placeholder="CNAM d'arrivé" required>
+                </div>
+                <div class="col">
+                    <input type="date" placeholder="Date" required>
+                </div>
+                <div class="col">
+                    <input type="number" placeholder="Nombre de place" required>
+                </div>
+                <div class="col">
+                    <button type="submit">Rechercher</button>
+
+                </div>
+            </div>
+
+
+        </form>
+    </div>
+
+
+
+    <div class="row">
+        <div class="justify-content-center row">
+            <h2>Trouver le covoiturage qui correspond à vos besoin</h2>
+            <p>Besoin d'un covoiturage pour se rendre dans un centre du CNAM? BLABLACNAM est là pour vous!</p>
+            <h2></h2>
         </div>
-      <div class="col-md-8">
-          <div class="justify-content-center row">
-              <h2>Trouver le covoiturage qui correspond à vos besoin</h2>
-              <p>Besoin d'un covoiturage pour se rendre dans un centre du CNAM? BLABLACNAM est là pour vous!</p>
-              <h2></h2>
-          </div>
-    <div class="row justify-content-center">
-        <img class="img-accueil " src="public/covoit.jpg" alt="">
+        <div class="row justify-content-center">
+            <img class="img-accueil " src="public/covoit.jpg" alt="">
+
+        </div>
 
     </div>
-        
-            </div>
-
-        </div>
 
 
 
