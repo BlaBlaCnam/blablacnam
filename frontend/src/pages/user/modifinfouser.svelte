86d42fb9e0f36aaa4c5349f00f3d442dc0cbe772
oceane tardy <tardy.oceane42@gmail.com>
2023-05-24 19:55:16 +0200

    Modif infouser

--- frontend/src/pages/user/modifinfouser.svelte
+++ frontend/src/pages/user/modifinfouser.svelte
@@ -1,30 +1,35 @@
-
- <h1>Modification Informations utilisateurs</h1>
+<h1>Modification Informations utilisateurs</h1>
 
 <div class="info-container">
     <h2 class="info-heading">Information utilisateurs</h2>
     <form>
-      <input class="inputpage" type="text" placeholder="Nom" required>
-      <input class="inputpage" type="text" placeholder="Prénom" required>
-      <input class="inputpage" type="text" placeholder="Téléphone" required>
-      <input class="inputpage" type="date" placeholder="Date de naissance" required>
-      <input class="inputpage" type="text" placeholder="Site de formation" required>
+        <input class="inputpage" type="text" placeholder="Nom" required>
+        <input class="inputpage" type="text" placeholder="Prénom" required>
+        <input class="inputpage" type="text" placeholder="Téléphone" required>
+        <input class="inputpage" type="date" placeholder="Date de naissance" required>
+        <input class="inputpage" type="text" placeholder="Site de formation" required>
 
-      <div class="row">
-        <div class="col">
-          <div class="bouton-user">
-            <button class="btnpage" type="submit">Valider</button>      
-          </div>
+        <div class="row">
+            <div class="col-md">
+                <div class="bouton-user">
+                    <button class="btnpage" type="submit">Valider</button>
+                  <a href="/user/infouser">
+                    <button class="btnpage" type="submit">Retour</button>
+                  </a>
+                </div>
+            </div>
+
+            <div class="col-md">
+                <div class="bouton-user">
+                    <a href="/identification/modifpassword">
+                        <button class="btnpage">Modifier mot de passe</button>
+                    </a>
+                </div>
+            </div>
         </div>
-        <div class="col">
-          <div class="bouton-user">
-            <a href="/identification/modifpassword"><button class="btnpage">Modifier mot de passe</button></a>
-          </div>
-        </div>
-      </div>
-       
-    </form> 
-  </div>
+
+    </form>
+</div>
   
 
 
