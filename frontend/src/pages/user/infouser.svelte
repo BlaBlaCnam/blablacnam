855c312e5ceeb0720c690cd7e7d1d964ed64ce32
oceane tardy <tardy.oceane42@gmail.com>
2023-05-24 16:22:33 +0200

    Reprise infos

--- frontend/src/pages/user/infouser.svelte
+++ frontend/src/pages/user/infouser.svelte
@@ -1,22 +1,34 @@
 
 
 <div class="info-container">
-    <h2 class="info-heading">Information utilisateurs</h2>
+  <div class="row">
+    <div class="col-6">
+
+      <h2 class="info-heading">Information utilisateurs</h2>
       <p> Nom: </p>
       <p> Prénom: </p>
       <p> Téléphone: </p>
       <p> Date de Naissance: </p>
       <p> Site de formatiion : </p>
-    <h3>Adresse</h3>
-          <p> Numéro: </p>
+      <h3>Adresse</h3>
+      <p> Numéro: </p>
       <p> Rue: </p>
       <p> Code postal: </p>
       <p> Ville: </p>
 
       <div class="bouton-user">
         <a href="/user/modifinfouser"><button class="btnpage" > Modifier mes données</button></a>
-        <button class="btnpage">Supprimer le compte</button>   
+        <button class="btnpage">Supprimer le compte</button>
       </div>
+    </div>
+
+    <div class="col-6">
+      <h2>Mes trajets</h2>
+    </div>
+
+
+  </div>
+
   </div>
 
 
