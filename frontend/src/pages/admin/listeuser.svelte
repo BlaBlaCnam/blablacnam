1d9f1d38f2d91eefada4a7878dd7b82cb722f713
unknown <Matthias BEAUCHAUD>
2023-05-24 15:18:08 +0200

    Liste user

---
+++ frontend/src/pages/admin/listeuser.svelte
@@ -0,0 +1,29 @@
+<div class="listeuser-container">
+    <h2 class="listeuser-body">Liste des utilisateurs :</h2>
+    <form class="systeme-recherche" action="/recherche" method="get">
+        <input class="recherche-user" type="text" name="q" placeholder="Recherche">
+        <button class="bouton-recherche" type="submit">Rechercher</button>
+    </form>
+
+    <table class="table-utilisateur">
+        <tr class="tr-utilisateur">
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Email</th>
+        </tr>
+        <tr class="tr-utilisateur">
+            <td>Smith</td>
+            <td>John</td>
+            <td>john.smith@example.com</td>
+        </tr>
+        <tr class="tr-utilisateur">
+            <td>Doe</td>
+            <td>Jane</td>
+            <td>jane.doe@example.com</td>
+        </tr>
+        <tr class="tr-utilisateur">
+            <td>Thomas</td>
+            <td>Nathan</td>
+            <td>NathanThomasAnalDestroyer@example.com</td>
+        </tr>
+</div>
