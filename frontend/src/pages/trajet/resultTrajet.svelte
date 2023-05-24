687bbed76660c70853310aab2929e0e31e2de8d1
oceane tardy <tardy.oceane42@gmail.com>
2023-05-24 20:08:32 +0200

    Ajout page resultat de recherche

---
+++ frontend/src/pages/trajet/resultTrajet.svelte
@@ -0,0 +1,31 @@
+<div class="container">
+    <h2 class="">Résultat de la recherche : </h2>
+
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Date</th>
+            <th>Lieu de départ</th>
+            <th>Lieu d'arrivée</th>
+            <th>Infos</th>
+        </tr>
+        </thead>
+        <tbody>
+        <!-- Ajoutez vos lignes de données ici -->
+        <tr>
+            <td>01/05/2023</td>
+            <td>Paris</td>
+            <td>Lyon</td>
+            <td>+</td>
+        </tr>
+        <tr>
+            <td>05/05/2023</td>
+            <td>Lyon</td>
+            <td>Marseille</td>
+            <td>+</td>
+        </tr>
+        <!-- ... et ainsi de suite pour chaque trajet -->
+        </tbody>
+    </table>
+</div>
