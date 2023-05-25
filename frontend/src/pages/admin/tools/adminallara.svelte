d7bfa19a6d1a4574d0a4f6fcc0ad574cadd260aa
thomas <thomas.vessiller.tv@gmail.com>
2023-05-25 10:56:21 +0200

    Tools

---
+++ frontend/src/pages/admin/tools/adminallara.svelte
@@ -0,0 +1,34 @@
+<div class="listeuser-container">
+    <h2 class="listeuser-body">Liste des codes ARA :</h2>
+    <form class="systeme-recherche" action="/recherche" method="get">
+        <input class="recherche-user" type="text" name="q" placeholder="Recherche">
+        <button class="bouton-recherche" type="submit">Rechercher</button>
+    </form>
+
+
+        <table class="table-utilisateur">
+            <thead>
+                <tr>
+                    <th>n°ara</th>
+                    <th>nom</th>
+                    <th>prenom</th>
+                    <th>voir</th>
+                    <th>Supprimer</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>CNAM</td>
+                    <td>Saint etienne</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Lyon</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+</div>
---
+++ frontend/src/pages/admin/tools/adminallsections.svelte
@@ -0,0 +1,36 @@
+<div class="listeuser-container">
+    <h2 class="listeuser-body">Liste des sections :</h2>
+    <form class="systeme-recherche" action="/recherche" method="get">
+        <input class="recherche-user" type="text" name="q" placeholder="Recherche">
+        <button class="bouton-recherche" type="submit">Rechercher</button>
+    </form>
+
+
+        <table class="table-utilisateur">
+            <thead>
+                <tr>
+                    <th>nom</th>
+                    <th>Voir</th>
+                    <th>Supprimer</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Smith</td>
+                    <td>John</td>
+                    <td>hello world</td>
+                    <td>20/10/2019</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Jane</td>
+                    <td>very useful message</td>
+                    <td>20/10/2019</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+</div>
