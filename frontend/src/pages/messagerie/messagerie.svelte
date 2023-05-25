5c957cf0d8131a4e55bec6180bbb3bc5bffb04cf
oceane tardy <tardy.oceane42@gmail.com>
2023-05-25 15:36:49 +0200

    Ajout page messagrie

---
+++ frontend/src/pages/messagerie/messagerie.svelte
@@ -0,0 +1,38 @@
+<div class="result-container">
+
+    <a href="/user/infouser">
+        <button class="btnpage" type="submit">Retour</button>
+    </a>
+    <h2 class="listeuser-body">Messagerie :</h2>
+
+    <table class="table-utilisateur">
+        <thead>
+        <tr>
+            <th>N°ARA expediteur</th>
+            <th>N°ARA destinataire</th>
+            <th>Contenu du message</th>
+            <th>Date du message</th>
+            <th>Voir</th>
+            <th>Supprimer</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Smith</td>
+            <td>John</td>
+            <td>hello world</td>
+            <td>20/10/2019</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Doe</td>
+            <td>Jane</td>
+            <td>very useful message</td>
+            <td>20/10/2019</td>
+            <td></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
