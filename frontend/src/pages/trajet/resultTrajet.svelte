bf331380e47ded6d04e33ff4b85ec74de5e322fc
oceane tardy <tardy.oceane42@gmail.com>
2023-05-25 10:40:20 +0200

    Reprise page résultat de recherche

--- frontend/src/pages/trajet/resultTrajet.svelte
+++ frontend/src/pages/trajet/resultTrajet.svelte
@@ -1,31 +1,40 @@
-<div class="container">
-    <h2 class="">Résultat de la recherche : </h2>
+<div class="result-container">
+    <div class="row">
+        <div class="col-md-8">
+            <h2 class="">Résultat de la recherche : </h2>
 
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Lieu de départ</th>
+                    <th>Lieu d'arrivée</th>
+                    <th>Infos</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>01/05/2023</td>
+                    <td>Paris</td>
+                    <td>Lyon</td>
+                    <td><img src="/eye.png"></td>
+                </tr>
+                <tr>
+                    <td>05/05/2023</td>
+                    <td>Lyon</td>
+                    <td>Marseille</td>
+                    <td><img src="/eye.png"></td>
+                </tr>
+                <!-- ... et ainsi de suite pour chaque trajet -->
+                </tbody>
+            </table>
 
-    <table class="table">
-        <thead>
-        <tr>
-            <th>Date</th>
-            <th>Lieu de départ</th>
-            <th>Lieu d'arrivée</th>
-            <th>Infos</th>
-        </tr>
-        </thead>
-        <tbody>
-        <!-- Ajoutez vos lignes de données ici -->
-        <tr>
-            <td>01/05/2023</td>
-            <td>Paris</td>
-            <td>Lyon</td>
-            <td>+</td>
-        </tr>
-        <tr>
-            <td>05/05/2023</td>
-            <td>Lyon</td>
-            <td>Marseille</td>
-            <td>+</td>
-        </tr>
-        <!-- ... et ainsi de suite pour chaque trajet -->
-        </tbody>
-    </table>
+        </div>
+
+        <div class="col-md-4">
+            <img src="/loupe.jpg" alt="" class="img-accueil">
+        </div>
+
+    </div>
+
 </div>
