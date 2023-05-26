fa8d45ef9e4c9d56be0b74986280ed8b285f12ba
ElTracThor <90599499+ElTracThor@users.noreply.github.com>
2023-05-26 11:17:48 +0200

    numero ara login

--- backend/nginx/html/get_users.php
+++ backend/nginx/html/get_users.php
@@ -4,7 +4,7 @@ require_once ".config.php";
 
 global $db;
 
-$query = $db->prepare("select UTILISATEUR.*, SECTION.nom as section, SITE.nom as site from UTILISATEUR, SECTION, SITE WHERE UTILISATEUR.id_section = SECTION.id_section and UTILISATEUR.id_site = SITE.id_site order by id_utilisateur desc;");
+$query = $db->prepare("select UTILISATEUR.*, SECTION.nom as section, SITE.nom as site from UTILISATEUR , SECTION, SITE WHERE UTILISATEUR.id_section = SECTION.id_section and UTILISATEUR.id_site = SITE.id_site order by id_utilisateur desc;");
 $query->execute();
 
 echo json_encode($query->fetchAll());
