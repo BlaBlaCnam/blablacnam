bf1d8c0b81d1e18c0ee1e05a8782db34dd01fbaf
ElTracThor <90599499+ElTracThor@users.noreply.github.com>
2023-05-26 10:51:03 +0200

    ajout message

--- frontend/src/pages/messagerie/postmessage.svelte
+++ frontend/src/pages/messagerie/postmessage.svelte
@@ -3,28 +3,26 @@
     import { api_submit } from "../../function";
     import { api } from "../../config";
 
-
     let users = [];
 
     onMount(async () => {
         users = await api_fetch(api + "/get_users.php");
     })
-
 </script>
-<div class="row message-user">
 
-    <form>
-        <input type="hidden" name="id_utilisateur_expediteur">
+<div class="row message-user">
+    <form method="POST" action={api + "/post_message.php"} on:submit={api_submit(() => {
+        window.history.pushState({}, '', '/messagerie/messagerie');
+      })}>
         <select name="destinataire" class="inputCenter" required>
             <option value="-1" disabled selected>Choissisez un destinataire</option>
             {#each users as user}
-                <option value={user.id_utilsateur}>{user.nom}-{user.prenom}</option>
+                <option value={user.id_utilisateur}>{user.nom}-{user.prenom}</option>
             {/each}
         </select>
-        <input type="hidden" name="date">
 
         <label>Mon message:</label>
-        <input class="inputCenter" name="contenu" type="text" placeholder="Entrer votre message" required>
+        <input class="inputCenter" id = "contenu" name="contenu" type="text" placeholder="Entrer votre message" required>
         <div class="row">
             <div class="col-md">
                 <div class="bouton-modifpassword">
