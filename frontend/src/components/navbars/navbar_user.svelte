7c0d274ba0ecb75ab43f890a15c7bec007f818b6
thomas <thomas.vessiller.tv@gmail.com>
2023-05-26 12:35:43 +0200

    v

---
+++ frontend/src/components/navbars/navbar_user.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { user } from "../../stores/user"
+    import { api_fetch } from "../../function";
+    import { api } from "../../config";
+</script>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><img src="/BLABLACNAM.png" class="navbar-logo" /></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="/user/infouser">Mes informations</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link active" href="/trajet/mesTrajets">Mes trajets</a>
+        </li>
+      </ul>
+      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <li>
+          <a class="dropdown-toggle nav-link active" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">{$user.nom}</a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#" on:click={async () => {
+              await user.set(null);
+              await api_fetch(api + "/logout.php");
+              window.history.pushState({}, '', '/');
+            }}>Déconnexion</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
